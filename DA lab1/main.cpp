32ddeab7016e313d62a1963be10dda15693a2d29
Whitewolf185 <matvey2001xxl1976@mail.ru>
2020-10-02 14:43:54 +0300

    PROBLEM: cannot access memory

--- DA lab1/main.cpp
+++ DA lab1/main.cpp
@@ -1,11 +1,36 @@
-#include <iostream>
 #include "T_Vector.h"
+#include<string>
+
+struct T_Pair{
+    short key[6];
+    std::string val;
+};
+
+void Convert_string_to_key (T_Pair &tmp, const std::string &str){
+    for (int i = 0; i < 6; ++i) {
+        tmp.key[i] = str[i] - '0';
+    }
+}
 
 int main() {
-    N_Vector::T_Vector<int> a(5, 11);
-    a.Push_back(45);
+    N_Vector::T_Vector<T_Pair> values;
 
-    std::cout << a.Get_size() << "   " << a[a.Get_size() - 1] << "  " << a[a.Get_size() - 2] << std::endl;
+
+    std::string str_key, str_val;
+    while(scanf("%s %s", &str_key, &str_val)){
+        T_Pair initial_val;
+        Convert_string_to_key(initial_val, str_key);
+        initial_val.val = str_val;
+
+        values.Push_back(initial_val);
+    }
+
+    for (int i = 0; i < values.Get_size(); ++i) {
+        for (auto j : values[i].key){
+            std::cout << j;
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
