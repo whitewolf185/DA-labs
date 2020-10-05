f1bce414947401f0c8c821edfd261ffad29b1a36
Whitewolf185 <matvey2001xxl1976@mail.ru>
2020-10-05 12:18:18 +0300

    vector now in main

--- DA lab1/main.cpp
+++ DA lab1/main.cpp
@@ -1,5 +1,94 @@
-#include "TVector.h"
 #include<string>
+
+#include <iostream>
+#include <stdio.h>
+namespace NVector {
+
+
+    template<class T>
+    class TVector {
+    private:
+        unsigned int size;
+        unsigned int capacity;
+        T *data;
+
+
+
+    public:
+
+        void Assign(const T elem){
+            for (unsigned int i = 0; i < size; ++i){
+                data[i] = elem;
+            }
+        }
+
+        TVector() : size(0), capacity(1) {
+            //std::cout << "TVector was created" << std::endl;
+            //data = NULL;
+        };
+
+        TVector(const unsigned int n) /*: TVector()*/ {
+            size = n;
+            capacity = n;
+            data = new T[capacity];
+        };
+
+        TVector(const unsigned int n, T elem)/*: TVector()*/{
+            size = n;
+            capacity = n;
+            data = new T[capacity];
+            Assign(elem);
+        }
+
+        unsigned int GetSize(){
+            return size;
+        }
+
+        void PushBack(const T &elem){
+            if(capacity == 1){
+                capacity *= 2;
+                data = new T[capacity];
+                data[size] = elem;
+                size++;
+            }
+
+            else if(capacity <= size){
+                capacity *= 2;
+                T* newData = new T[capacity];
+                for (int i = 0; i < size; ++i) {
+                    newData[i] = data[i];
+                }
+                delete[] data;
+                data = newData;
+                data[size] = elem;
+                size++;
+            }
+
+            else{
+                data[size] = elem;
+                size++;
+            }
+        }
+
+        bool Empty(){
+            return !size;
+        }
+
+        T &operator[](const unsigned int &iterator){
+            if(iterator >= size) {
+                std::cout << "CAUTION: size was reached\niterator is " << iterator << std::endl;
+                return data[size - 1];
+            }
+            return data[iterator];
+        }
+
+        ~TVector(){
+            if(!this->Empty()) {
+                delete[] data;
+            }
+        }
+    };
+}
 
 //#define DEBUG
 #define TEST_ENTER
