e6c1efb3251b3e383a60b926206bbe27409a4d3a
whitewolf185 <whitewolf.mot185@gmail.com>
2021-03-28 22:06:31 +0300

    Переделал все на умные указатели. Добавил геттеры. Расставил комментарии к каждой функции. TODO нужны для пометки где могут быть ошибки

--- DA lab5/BORis.h
+++ DA lab5/BORis.h
@@ -27,26 +27,28 @@ private:
     unsigned int begin;
     std::shared_ptr<unsigned> last;
     bool leaf = true;
-    std::unique_ptr<TBorNode> url;
-//    std::unique_ptr<TBorNode> prev;
-    std::unordered_map<char, TBorNode *> next;
+    std::shared_ptr<TBorNode> url;
+    std::shared_ptr<TBorNode> prev;
+    std::unordered_map<char, std::shared_ptr<TBorNode>> next;
     unsigned id;
 
     //constructors
     //Конструктор. Первые 2 аргумента всегда должны быть одни и те же. Последние 2 - начало и общий конец.
-    TBorNode(TBorNode *root, unsigned &globID, const unsigned &_begin, const std::shared_ptr<unsigned> &end) {
+    //По умолчанию все новые ноды создаются со ссылкой на root.
+    TBorNode(const std::shared_ptr<TBorNode> &root, unsigned &globID, const unsigned &_begin,
+             const std::shared_ptr<unsigned> &end) {
       id = globID++;
       last = end;
       begin = _begin;
-      url.reset(root);
+      url = root;
     }
 
     //Конструктор с задаваемой в конце end-ом, как число. Нужен для вставки ноды не как лист.
-    TBorNode(TBorNode *root, unsigned &globID, const unsigned &_begin, const unsigned &end) {
+    TBorNode(const std::shared_ptr<TBorNode> &root, unsigned &globID, const unsigned &_begin, const unsigned &end) {
       id = globID++;
       last.reset(new unsigned(end));
       begin = _begin;
-      url.reset(root);
+      url = root;
       leaf = false;
     }
 
@@ -66,24 +68,31 @@ private:
 
     //Нужна для вставки следующего элемента. В аргументах пара,
     // в которой должно содержаться первая буква и указатель со вставляемой нодой.
-    void Insert(const std::pair<char, TBorNode *> &tmp_pair) {
+    void Insert(const std::pair<char, std::shared_ptr<TBorNode>> &tmp_pair) {
       next.insert(tmp_pair);
       leaf = false;
     }
 
     //Ф получает ноду по символу. Нужно, чтобы доставать нужную ноду на развилке.
     // Обычно используется в Ф Next
-    TBorNode *GetNodeElem(const char &c) {
+    std::shared_ptr<TBorNode> GetNodeElem(const char &c) {
       auto tmp = next.find(c);
       if (tmp == next.end()) {
         return nullptr;
       }
       return tmp->second;
     }
+
+    void Copy(const std::shared_ptr<TBorNode> &cmp) {
+      last = cmp->last;
+      leaf = cmp->leaf;
+      next = cmp->next;
+    }
+
   };
 
 protected:
-  TBorNode *root;
+  std::shared_ptr<TBorNode> root;
 
 
 //----functions
@@ -101,14 +110,10 @@ private:
 public:
   TBoris() {
     end.reset(new unsigned int(0));
-    root = new TBorNode(root, globID, 0, end);
+    root.reset(new TBorNode(root, globID, 0, end));
   };
 
-  ~TBoris() {
-    for (auto &item : root->next) {
-      delete item.second;
-    }
-  }
+  ~TBoris() = default;
 
   void SetText(const std::string &str1, const std::string &str2) {
     texts = str1 + FANTOM_CHAR1 + str2 + FANTOM_CHAR2;
@@ -119,18 +124,15 @@ public:
   private:
     int reminder = 0;
     int activeLen = 0;
-    std::unique_ptr<TBorNode> prevUrl;
-    std::unique_ptr<TBorNode> activeNode;
+    std::shared_ptr<TBorNode> prevNode;
+    std::shared_ptr<TBorNode> activeNode;
 
   public:
-    TIterator() {
-      prevUrl = nullptr;
-      activeNode = nullptr;
-    }
+    TIterator() = delete;
 
-    TIterator(TBorNode *node) {
-      activeNode.reset(node);
-      prevUrl = nullptr;
+    TIterator(std::shared_ptr<TBorNode> node) {
+      activeNode = node;
+      prevNode = nullptr;
     }
 
     ~TIterator() = default;
@@ -138,13 +140,15 @@ public:
     //Ф перехода на следующую ноду
     void Next(const char &c) {
       if (activeNode == nullptr) {
-        throw std::logic_error("Trying to call nullptr");
+        throw std::logic_error("Trying to call nullptr in Next");
       }
 
-      TBorNode *tmp = activeNode->GetNodeElem(c);
+      auto tmp = activeNode->GetNodeElem(c);
       if (tmp != nullptr) {
-        activeNode.reset(tmp);
+        activeNode = tmp;
+        return;
       }
+      throw std::invalid_argument("Cannot find elem in Next");
     }
 
     //Ф перехода по суфф ссылке
@@ -153,16 +157,68 @@ public:
         throw std::logic_error("Trying to call nullptr in URL");
       }
 
-      prevUrl = std::move(activeNode);
-      activeNode = std::move(activeNode->url);
+      //TODO возможно тут ошибка, связанная с тем, что prevNode = activNode->url
+      prevNode = activeNode;
+      activeNode = activeNode->url;
+    }
+
+    //Ф нужна для того, чтобы разделить ноду на 2. С _ идут аргументы обязательные для создания новой ноды.
+    //Потом идут begin - индекс символа нового элемента, splitter - индекс символа сплитуемого элемента.
+    //Стоит также отметить, что после сплита я остаюсь на той же ноде, что и был. Никакого перехода нет.
+    void Split(const std::shared_ptr<TBorNode> &_root, unsigned int &_globID, const std::string &_texts,
+               const std::shared_ptr<unsigned> &_end,
+               const unsigned int &begin, const unsigned &splitter) {
+
+      //TODO устранил возможную ошибку с разделением не в листе. Может появится ошибка при копировании.
+      //старый элемент
+      auto oldNode = std::make_shared<TBorNode>(_root, _globID, splitter + 1, _end);
+      oldNode->Copy(activeNode);
+      activeNode->last.reset(new unsigned(splitter));
+      activeNode->leaf = false;
+      activeNode->Insert(std::pair<char, std::shared_ptr<TBorNode>>(_texts[splitter + 1],
+                                                                    oldNode));
+      //новый элемент
+      activeNode->Insert(std::pair<char, std::shared_ptr<TBorNode>>(_texts[begin],
+                                                                    std::make_shared<TBorNode>(_root, _globID, begin,
+                                                                                               _end)));
+    }
+
+    //отладочная функция
+    void PrintNode(const std::string &_texts) {
+      activeNode->PrintNode(_texts);
+      std::cout << std::endl;
     }
 
   };
 //--------------------------END OF ITERATOR--------------------------
 
+//getters
+  std::shared_ptr<TBorNode> GetRoot() const {
+    return root;
+  }
+
+  unsigned int &GetID() {
+    return globID;
+  }
+
+  std::string GetTexts() const {
+    return texts;
+  }
+
+  std::shared_ptr<unsigned> GetEnd() const {
+    return end;
+  }
+
+
+  //Ф нужна для инкрементирования end
+  void IncEnd() {
+    ++*end;
+  }
+
   void Test() {
-    *end = texts.size() - 1;
-    root->Insert(std::pair<char, TBorNode *>(texts[0], new TBorNode(root, globID, 0, end)));
+    *end = texts.size() - 2;
+    root->Insert(
+            std::pair<char, std::shared_ptr<TBorNode>>(texts[0], std::make_shared<TBorNode>(root, globID, 0, end)));
 
     TestPrinter();
   }
--- DA lab5/main.cpp
+++ DA lab5/main.cpp
@@ -1,1 +1,52 @@
+//#define TRY_BOR
+
+#ifdef TRY_BOR
 #include "BORis.h"
+#endif
+
+#ifndef TRY_BOR
+
+#include <iostream>
+#include <memory>
+
+#endif
+
+int main() {
+#ifdef TRY_BOR
+  TBoris a;
+  try{
+    std::string str1 = "hello";
+    std::string str2 = "hi";
+    a.SetText(str1,str2);
+    a.Test();
+    TBoris::TIterator iter(a.GetRoot());
+
+    iter.Next('h');
+    iter.Split(a.GetRoot(), a.GetID(),a.GetTexts(),a.GetEnd(),
+               a.GetTexts().size() - 1,3);
+
+    iter.PrintNode(a.GetTexts());
+    iter.Next(FANTOM_CHAR2);
+    iter.PrintNode(a.GetTexts());
+  }
+  catch (const std::invalid_argument& e){
+    std::cout << e.what();
+  }
+  catch (const std::logic_error& e){
+    std::cout << e.what();
+  }
+#endif
+
+#ifndef TRY_BOR
+  int *tmp = new int(1);
+  std::unique_ptr<int> a(new int(2345));
+  std::unique_ptr<int> b = std::move(a);
+  a.reset(tmp);
+  ++*a;
+
+  std::cout << "a = " << *a << std::endl;
+  std::cout << "b = " << *b << std::endl;
+#endif
+
+  return 0;
+}
